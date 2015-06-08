8e653712a165293dc5eab68d43e68ad2784f4a4b
danielturek <dturek@berkeley.edu>
2015-06-08 15:51:48 -0700

    added test_that statements

--- test.R
+++ test.R
@@ -4,7 +4,15 @@ a
 warning('this is the warning message')
 b <- 1 + 2
 print(b)
+test_that(
+    'test_that message which should pass', {
+    expect_that(sin(pi / 4), equals(1 / sqrt(2)))
+})
 c <- 1 + 3
 print(c)
-
+test_that('test_that message which should FAIL', {
+    expect_that(sin(pi / 4), equals(1))
+})
+d <- 1 + 4
+print(d)
 
