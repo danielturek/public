009447e33f37b0166d4020aa671d4cb391993928
danielturek <dturek@berkeley.edu>
2015-06-22 11:27:37 -0700

    changed test.R model so it should work

--- test.R
+++ test.R
@@ -1,4 +1,3 @@
-library(igraph)
 library(nimble)
 
 a <- 1 + 1
@@ -17,12 +16,14 @@ d <- 1 + 4
 print(d)
 
 code <- nimbleCode({
-     a ~ dnorm(0, 1)
+    a ~ dnorm(0, 1)
+    b ~ dnorm(a, 1)
 })
 
 constants <- list()
-data <- list()
-inits <- list()
+data <- list(b = 1)
+inits <- list(a = 0)
+
 Rmodel <- nimbleModel(code, constants, data, inits)
 
 spec <- configureMCMC(Rmodel)
@@ -36,3 +37,4 @@ set.seed(0)
 Cmcmc$run(10000)
 samples <- as.matrix(Cmcmc$mvSamples)
 apply(samples, 2, mean)
+
