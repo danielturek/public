5f097149cb01945a916af09c33b62220c7e7079c
danielturek <dturek@berkeley.edu>
2015-11-11 10:45:09 -0800

    added penguin data

--- whitman/demo.R
+++ whitman/demo.R
@@ -2,7 +2,18 @@
 ##publish: true
 ##---
 
+##### Worked exercise using R
+##### Penguin tagging data
+
 ##```{r}
-a <- 1
+n <- c(167, 189)
+y <- c(33, 68)
+p <- y/n
+
+n1 <- n[1];    n2 <- n[2]
+y1 <- y[1];    y2 <- y[2]
+p1 <- p[1];    p2 <- p[2]
 ##```
+
+
 
